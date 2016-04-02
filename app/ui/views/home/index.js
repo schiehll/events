491dde2c59173c716f96196b071bebd73ffff3d7
schiehll <rn.schiehll@gmail.com>
2016-04-01 21:19:43 -0300

    clean up tag filter

--- app/ui/views/home/index.js
+++ app/ui/views/home/index.js
@@ -52,8 +52,26 @@ class Home extends View {
     }))
   }
 
+  showSubtitle() : Object {
+    const {subtitle} = this.state
+    if(subtitle.filter !== null){
+      return(
+        <span>
+          {i18n.t(subtitle.key, {tag: subtitle.filter})} 
+          <FontIcon 
+            onClick={this.clearFilter.bind(this)} 
+            color={colors.lightPrimary} 
+            className={`material-icons ${styles.clear}`}
+          >clear</FontIcon>
+        </span>
+      )
+    }
+    
+    return <span>{i18n.t(subtitle.key)}</span>
+  }
+
   onRender() : Object {
-    const {auth, tab, events, subtitle} = this.state
+    const {auth, tab, events} = this.state
     return(
       <div className={styles.main}>
         <Header 
@@ -61,10 +79,7 @@ class Home extends View {
           onLogout={this.logout.bind(this)}
         />
         <h2 className={styles.title}>{i18n.t(tab)}</h2>
-        <h3 className={styles.subtitle}>{
-          subtitle.filter !== null 
-          ? <span>{i18n.t(subtitle.key, {tag: subtitle.filter})} <FontIcon onClick={this.clearFilter.bind(this)} color={colors.lightPrimary} className={`material-icons ${styles.clear}`}>clear</FontIcon></span> : i18n.t(subtitle.key)
-        }</h3>
+        <h3 className={styles.subtitle}>{this.showSubtitle.bind(this)()}</h3>
         <Tabs className={styles.tabs} tabItemContainerStyle={{width: 200}}>
           <Tab 
             label={i18n.t('TAB_LIST')} 
