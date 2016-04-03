6535931896807139a0b4059abcbc16c184b1c657
schiehll <rn.schiehll@gmail.com>
2016-04-03 06:06:34 -0300

    add auth loader

--- app/ui/components/auth-loader/index.js
+++ app/ui/components/auth-loader/index.js
@@ -1,11 +1,14 @@
+import styles from '+/assets/styles/components/auth-loader.pcss'
+import colors from '+/assets/styles/variables/colors.pcss'
 import React from 'react'
 import DumbComponent from '+/core/DumbComponent'
+import {CircularProgress} from 'material-ui'
 
 class AuthLoader extends DumbComponent {
   onRender() : Object {
     return(
-      <div>
-        Authenticating...
+      <div className={styles.loader}>
+        <CircularProgress color={colors.lightPrimary} />
       </div>
     )
   }
